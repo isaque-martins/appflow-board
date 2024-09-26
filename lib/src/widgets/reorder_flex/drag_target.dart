25086f7436ebfb3523cbbd3ebdfcbdf9ec6ba6c2
isaquemartins <isaque-martins@live.com>
2024-09-26 20:21:29 -0300

    Andon 147 - Review dartFix2

--- lib/src/widgets/reorder_flex/drag_target.dart
+++ lib/src/widgets/reorder_flex/drag_target.dart
@@ -135,7 +135,7 @@ class _ReorderDragTargetState<T extends DragTargetData>
         }
       },
     );
-    //Teste1
+    //Teste2
 
     dragTarget = KeyedSubtree(key: widget.indexGlobalKey, child: dragTarget);
     return dragTarget;
